bfa3b8b1922456c29b9e9cf0b231c0b65c974aac
cllorca1 <carlos.llorca@tum.de>
2017-07-18 16:59:11 +0200

    added test framework

---
+++ src/test/java/MtoTest.java
@@ -0,0 +1,24 @@
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Created by carlloga on 7/18/2017.
+ */
+public class MtoTest {
+
+    private static final Logger log = Logger.getLogger(MtoTest.class);
+
+    @Test
+     public final void testMain(){
+
+
+
+
+
+    }
+
+
+
+
+
+}
